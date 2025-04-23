b4ec04400a680322e76813445ab8940e404998e4
neko <nekoday@proton.me>
2025-04-23 14:44:20 +0800

    🏅 修正 Licence 日期

--- Pixiv.js
+++ Pixiv.js
@@ -16,7 +16,7 @@
 /*
 MIT License
 
-Copyright (c) 2024 nekoday
+Copyright (c) 2025 nekoday
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
